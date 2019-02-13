ec9894099f424e0055b32c8e2a97c0f875eb5afc
papetine <papetine95@gmail.com>
2019-02-13 16:33:21 +0000

    nouveau coursController

--- src/Controller/CoursController.php
+++ src/Controller/CoursController.php
@@ -5,22 +5,41 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\CoursRepository;
+use App\Repository\CategorieRepository;
+use Symfony\Component\HttpFoundation\Response;
 
 class CoursController extends AbstractController
 {
     /**
-     * @Route("/cours", name="cours")
+     * @Route("/", name="allcours")
      */
-    public function allCours(CoursRepository $coursrepo)
+    public function allCours(CoursRepository $coursrepo, CategorieRepository $categorierepo)
     {
-        $product = $coursrepo->findAll();
-
+        $product = $coursrepo->findBy(['visibilite' => 1]);
+        $categories = $categorierepo->findAll();
         foreach ($product as $key => $value) {
             $value->setImage(base64_encode(stream_get_contents($value->getImage())));
+        }
+        foreach ($categories as $key => $value) {
+            $value->setImgC(base64_encode(stream_get_contents($value->getImgC())));
         }
 
         return $this->render('cours/allCours.html.twig', [
             'produits' => $product,
+            'categories' => $categories,
         ]);
+    }
+
+    /**
+     * @Route("/cours/{slug}", name="findByCategory")
+     */
+    public function findByCategory($slug, CoursRepository $coursrepo): Response
+    {
+        $product = $coursrepo->findOneBy(['slug' => $slug]);
+
+        //  return new Response('Check out this great product: '.$product->getDescription());
+        return $this->render('cours/detail.html.twig', [
+        'cours' => $product,
+    ]);
     }
 }
